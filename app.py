85eda375e6e6e9885c998835001ad75e0a5811aa
Eliyahu Kainyah Yalley <kofiyalley16@gmail.com>
2024-04-24 13:14:49 +0800

    initial streaming

--- app.py
+++ app.py
@@ -6,25 +6,32 @@ from langchain.prompts import PromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_community.vectorstores.zilliz import Zilliz
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
-from streamlit_chat import message
+from langchain.callbacks.base import BaseCallbackHandler
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text="", display_method='markdown'):
+        self.container = container
+        self.text = initial_text
+        self.display_method = display_method
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        display_function = getattr(self.container, self.display_method, None)
+        if display_function is not None:
+            display_function(self.text)
+        else:
+            raise ValueError(f"Invalid display_method: {self.display_method}")
 
 template = """
-        Beantworten Sie die Frage des Nutzers am Ende des Textes anhand der folgenden Informationen. 
-        Bei den Informationen handelt es sich um transkribierte Ausschnitte aus verschiedenen 
-        Podcast-Episoden über das Arbeiten in China. Die Fragen der Nutzer sollen nun nur auf der 
-        Grundlage der Antworten der Podcast-Gäste aus den Podcasts beantwortet werden. 
-        Wenn in den Informationen kein relevantes Wissen zur Beantwortung der Frage vorhanden ist,
-        sagen Sie bitte "Ich habe kein Podcast-Wissen zu dieser Frage". Andernfalls sollte die 
-        Ausgabe in Stichpunkten als Aufzählungspunkte erfolgen (jeder Aufzählungspunkt in einer Zeile) und der 
-        Name des Podcast-Gastes und der Name des Podcasts sollten in Klammern nach jedem Punkt im 
-        Format "(PODCAST NAME - PODCAST GAST)" stehen. Darüber hinaus sollte kein Ausgabetext angezeigt werden.
-        Wenn der Nutzer in Englisch seine Frage stellt, soll die Antwort auch ins Englische übersetzt werden.
+        Beantworten Sie die Frage des Nutzers am Ende des Textes anhand der folgenden Informationen. Bei den Informationen handelt es sich um transkribierte Ausschnitte aus verschiedenen Podcast-Episoden über das Arbeiten in China.
+        Die Fragen der Nutzer sollen nun nur auf der Grundlage der Antworten der Podcast-Gäste aus den Podcasts beantwortet werden. Wenn in den Informationen kein relevantes Wissen zur Beantwortung der Frage vorhanden ist, sagen Sie bitte "Ich habe kein Podcast-Wissen zu dieser Frage". Andernfalls sollte die Ausgabe in Stichpunkten als Aufzählungspunkte erfolgen (jeder Aufzählungspunkt in einer Zeile) und der Name des Podcast-Gastes und der Name des Podcasts sollten in Klammern nach jedem Punkt im Format "(PODCAST NAME - PODCAST GAST)" stehen. Am Anfang der Antwort soll hierbei stehen "Ich habe folgenden Informationen gefunden:"
+        Darüber hinaus sollte kein Ausgabetext angezeigt werden. Wenn der Nutzer in Englisch seine Frage stellt, soll die Antwort auch ins Englische übersetzt werden.
 
-{context}
+        {context}
 
-Frage: {question}
+        Frage: {question}
 
-Hilfreiche Antwort:
+        Hilfreiche Antwort:
 """
 custom_rag_prompt = PromptTemplate.from_template(template)
 
@@ -32,12 +39,6 @@ custom_rag_prompt = PromptTemplate.from_template(template)
 load_dotenv()
 
 embeddings = AzureOpenAIEmbeddings(model=os.environ.get("OPENAI_EMBEDDINGS_MODEL"))
-llm = AzureChatOpenAI(
-    deployment_name=os.environ.get("AZURE_OPENAI_CHAT_MODEL_DEPLOYMENT_NAME"),
-    temperature=0,
-    streaming=True,
-    max_tokens=4096,
-)
 connection_args = {
     "uri": os.environ.get("ZILLIZ_CLOUD_URI"),
     "token": os.environ.get("ZILLIZ_CLOUD_API_KEY"),
@@ -53,7 +54,7 @@ retriever = vectorstore.as_retriever()
 
 
 def main():
-    st.set_page_config(page_title="The China PodcastBot", layout="wide")
+    # st.set_page_config(page_title="The China PodcastBot", layout="centered")
     st.title('Podcast Analyst Genie: Ask every question about China')
     st.markdown(
         """
@@ -77,14 +78,21 @@ def main():
         with st.chat_message("user"):
             st.markdown(prompt)
         
-        rag_chain = (
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty() 
+            stream_handler = StreamHandler(message_placeholder, display_method='write')
+            llm = AzureChatOpenAI(
+                deployment_name=os.environ.get("AZURE_OPENAI_CHAT_MODEL_DEPLOYMENT_NAME"),
+                temperature=0,
+                streaming=True,
+                max_tokens=4096,
+                callbacks=[stream_handler],
+            )
+            rag_chain = (
                     {"context": retriever, "question": RunnablePassthrough()}
                     | custom_rag_prompt
                     | llm
                 )
-        
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty() 
             response = rag_chain.invoke(prompt).content
             message_placeholder.markdown(response + "|")
         message_placeholder.markdown(response)
